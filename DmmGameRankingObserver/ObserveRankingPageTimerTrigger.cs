a3bd62b0bd09c35e343e8c6ce2ff30b4da840da3
kokeiro <kokeiro.ssbm.001@gmail.com>
2017-10-09 17:38:35 +0900

    ランキングデータを監視するタイマートリガーを作成した

--- DmmGameRankingObserver/ObserveRankingPageTimerTrigger.cs
+++ DmmGameRankingObserver/ObserveRankingPageTimerTrigger.cs
@@ -1,20 +1,54 @@
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using DmmGameRankingObserver.Core;
+using Microsoft.Azure;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.WindowsAzure.Storage;
 
 namespace DmmGameRankingObserver
 {
     public static class ObserveRankingPageTimerTrigger
     {
+        static CloudStorageAccount StorageAccount =>
+            CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("AzureWebJobsStorage"));
+
+        static readonly string TableName = @"DmmGameRanking";
+
         [FunctionName("ObserveRankingPageTimerTrigger")]
         public static async Task Run([TimerTrigger("0 0 */1 * * *")]TimerInfo myTimer, TraceWriter log)
         {
             log.Info($"C# Timer trigger function executed at: {DateTime.Now}");
+            var nowStr = DateTime.Now.ToString("yyyyMMdd_hhmmss");
+            log.Info($"nowStr: {nowStr}");
 
-            //var client = new ArcanaHeart3lmsssRankingClient();
+            var dmmClient = new DmmGameRankingClient();
 
-            //var blobContainer = await GetBlobContainerAsync();
+            log.Info($"dmmClient.LoadFromWebAsync()");
+            await dmmClient.LoadFromWebAsync();
+
+            log.Info($"dmmClient.Parse()");
+            var rakingData = dmmClient.Parse();
+
+            log.Info($"new tableClient");
+            var tableClient = new DmmGameRankingTableClient(StorageAccount, TableName);
+
+            log.Info($"mapping rankingData to entity");
+            var entities = rakingData.Select(x => new DmmGameRankingEntity
+            {
+                GameTitle = x.Name,
+                ObservedDateTime = nowStr,
+                Rank = x.Rank,
+                Genre = x.Genre,
+                Comment = x.Comment,
+            });
+
+            log.Info($"tableClient.AddOrUpdateAsync(entities)");
+            await tableClient.AddOrUpdateAsync(entities);
+
+            log.Info($"finished!!");
         }
+
     }
 }
